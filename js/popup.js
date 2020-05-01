4a87421cbd91f5f9399c43838aee0583d602cabd
KaladinKholin <63601044+KaladinKholin@users.noreply.github.com>
2020-05-01 18:33:52 +0100

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,73 @@
+// Ventana emergente
+
+function start() {
+
+
+// Añadir libro
+
+const listaLibro = document.querySelector('.lista');
+
+eventListeners();
+
+function eventListeners() {
+
+
+
+    document.querySelector('#formulario').addEventListener('submit', 
+    agregarAlgo);
+
+}
+
+function agregarAlgo(e) {
+    e.preventDefault();
+    
+    const textoLista = document.querySelector('#input-text').value;
+
+    const libros = document.createElement('div');
+    libros.className += 'apunte';
+    const info = document.createElement('div');
+    info.className += 'info';
+    const imagen = document.createElement('img');
+    imagen.className += 'imagen';
+    imagen.src += 'music.png';
+    const pLibro = document.createElement('p');
+    pLibro.className += 'texto';
+    const sesamo = document.createElement('p');
+    sesamo.className += 'cerrar';
+
+    
+    
+    listaLibro.appendChild(libros);
+    libros.appendChild(info);
+    imagen.src = 'img/music.png';
+    info.appendChild(imagen);
+    pLibro.innerText = textoLista;
+    info.appendChild(pLibro);
+    sesamo.innerText = 'Eliminar';
+    libros.appendChild(sesamo);
+
+
+
+
+
+
+    // Eliminar libro
+
+var botones = document.getElementsByClassName('cerrar');
+
+for (var boton of botones) {
+  boton.addEventListener('click', function(e) {
+    e.preventDefault();
+    this.parentElement.remove();
+  });
+};
+    
+}
+
+
+  }
+  
+  window.onload = start;
+
+
+
